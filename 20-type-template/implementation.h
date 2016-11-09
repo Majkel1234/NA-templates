09a99c5096dd1b369e935d96031c1e944c9ee144
Michal Kawecki <majkel4306@wp.pl>
2016-11-09 15:54:31 +0100

    3rd commit

--- 20-type-template/implementation.h
+++ 20-type-template/implementation.h
@@ -1,5 +1,21 @@
 #pragma once
 
-template<typename T>
-class container_wrapper;
+#include <cstddef>
+#include <utility>
 
+template<typename T>
+class container_wrapper{
+public:
+	container_wrapper() = default;
+	container_wrapper(const container_wrapper &arg) = default;
+	container_wrapper(container_wrapper &&arg) = default;
+	container_wrapper& operator=(const container_wrapper &arg) = default;
+	container_wrapper& operator=(container_wrapper &&arg) = default;
+	container_wrapper(T arg): _value{std::move(arg)}{
+	}
+	size_t size() const{
+		return _value.size();
+	}
+private:
+   T _value;
+};
