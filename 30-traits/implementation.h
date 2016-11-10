2c70d29c031ab4cb7a5bb519a6d8a6d00cf46fa4
Michal Kawecki <majkel4306@wp.pl>
2016-11-10 12:49:10 +0100

    ..and another commit..

--- 30-traits/implementation.h
+++ 30-traits/implementation.h
@@ -1,5 +1,36 @@
 #pragma once
 
+#include <type_traits>
+
+
+	
+
 template<typename T, typename T1, typename T2, typename T3>
-struct is_any_of;
+struct is_any_of : std::disjunction<std::is_same<T,T1>,std::is_same<T,T2>,std::is_same<T,T3>>{	
+};			//disjunction - struktura, ktorej wartosc bazuje na jej argumentach
+				// - jesli pierwszy arg(is_same<T,T1>) dalej nie sprawdza i zwraca prawde
+
+
+
+
+
+
+	
+/* cwicz. 1
+template<typename T, typename T1, typename T2, typename T3>
+struct is_any_of : std::false_type {	
+};
+
+template<typename T, typename T1, typename T2>
+struct is_any_of<T,T,T1,T2> : std::true_type{
+};
+
+template<typename T, typename T1, typename T2>
+struct is_any_of<T,T1,T,T2> : std::true_type{
+};
+
+template<typename T, typename T1, typename T2>
+struct is_any_of<T,T1,T2,T> : std::true_type{
+}; */
+
 
