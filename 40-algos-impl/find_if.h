7dc98e5bdb30fd300ecbf52a51e37ba92d0fb70b
Michal Kawecki <majkel4306@wp.pl>
2016-11-10 15:51:52 +0100

    and another..

--- 40-algos-impl/find_if.h
+++ 40-algos-impl/find_if.h
@@ -1,8 +1,16 @@
 #pragma once
 
+
+
 namespace training
 {
 template<typename InputIt, typename UnaryPredicate>
-InputIt find_if(InputIt begin, InputIt end, UnaryPredicate pred);
+InputIt find_if(InputIt begin, InputIt end, UnaryPredicate pred){
+	InputIt iter;
+	for(iter = begin ;iter != end; ++iter){
+		if(pred(*iter)) break;	
+	}
+	return iter;
+   }
 }
 
--- 40-algos-impl/vector_erase.h
+++ 40-algos-impl/vector_erase.h
@@ -1,4 +1,16 @@
 #pragma once
 
+#include <algorithm>
+
 // define vector_erase here
+
+template <typename T>
+void vector_erase(T &vect, const typename T::value_type &val){
+/*	for(auto it = vect.begin(); it!= vect.end();++it){
+		if(*it == val){
+			vect.erase(it);
+		}
+	} */
+	vect.erase(std::remove(vect.begin(),vect.end(),val),vect.end());
+}
 
